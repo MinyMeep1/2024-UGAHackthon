7f3a6cb5855f66adc75e734874ea7449c25ee511
ChristopherGantes <78065304+ChristopherGantes@users.noreply.github.com>
2024-02-11 03:56:56 -0500

    fun times with my pals

--- preetam/face-api.js copy/examples/examples-browser/server.js
+++ preetam/face-api.js copy/examples/examples-browser/server.js
@@ -33,7 +33,7 @@ app.post('/fetch_external_image', async (req, res) => {
   }
 })
 
-app.listen(3000, () => console.log('Listening on port 3000!'))
+app.listen(3001, () => console.log('Listening on port 3001!'))
 
 function request(url, returnBuffer = true, timeout = 10000) {
   return new Promise(function(resolve, reject) {
